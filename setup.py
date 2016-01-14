57dcb23221e32596fdc4d7961a4b2764a2808b6a
Euan Thoms <euan@potensol.com>
2016-01-15 03:48:59 +0800

    Bumped version to 1.0rc2 in order to upload again to PyPi.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djaken',
-    version='1.0rc1',
+    version='1.0rc2',
     packages=['djaken'],
     include_package_data=True,
     license='BSD License',
