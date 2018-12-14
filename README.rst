05f43aac761ac53f71525c1b155bdc8883c7d178
Euan Thoms <euan@potensol.com>
2018-12-14 23:12:30 +0800

    Release prep for 2.0.

--- README.rst
+++ README.rst
@@ -7,6 +7,9 @@ Djaken is a complete web-based notes application for Django.
 News
 ----
 
+Version 2.0 released! (2018/12/14)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 Version 2.0rc1 released! (2018/12/06)
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djaken',
-    version='2.0rc1',
+    version='2.0',
     packages=['djaken'],
     include_package_data=True,
     license='BSD License',
